954a698c2f5ff34fa0b7ded3d22f5a9f8f2d88fe
charlesmarshall <cm56marshall@googlemail.com>
2011-06-14 12:05:24 +0100

    almost working plugin of the old campaignmonitor cms module suitable for v5. NOT WORKING FULLY

---
+++ setup.php
@@ -0,0 +1,9 @@
+<?
+CMSApplication::register_module("email", array("display_name"=>"Email", "link"=>"/admin/email/"));
+/*
+need to add to the config setup:
+campaign_monitor:
+  ClientID: XX
+  
+*/
+?>
